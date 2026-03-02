07bb32a91c352402b1e2b8d2751aef4a76eafd3a
Julio Cesar <jccintr@gmail.com>
2026-03-01 23:32:20 -0300

    antes de instalar chart.js

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_BASE_URL } from '../constants/api.constants';
+import { AuthService } from './auth.service';
+import { PedidosResponse } from '../models/pedidos/pedidos-response.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+   private readonly BASE_API: string = API_BASE_URL;
+
+   constructor(private http: HttpClient,private authService: AuthService) {}
+
+   getMonthReport(month:number,year:number) : Observable<PedidosResponse[]> {
+       return this.http.get<PedidosResponse[]>(`${this.BASE_API}/pedidos/${month}/${year}`, {
+         headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': 'Bearer ' + this.authService.token()
+         },
+       }); 
+     }
+}
